8d4875259d1984934c19a8c80fe874dbe38cdf58
mysrob <mysrob@gmail.com>
2021-01-08 22:47:35 +0100

    Remove accessory from cache

--- src/platform.ts
+++ src/platform.ts
@@ -36,39 +36,43 @@ export class Platform implements DynamicPlatformPlugin {
     const devices: Device[] = []; 
     api.forEach(apiItem => {
       devices.push({
-        uniqueId: `bi-temperature-sensor-${apiItem.name}`,
-        displayName: 'BI Temperature Sensor',
+        uniqueId: `bi-temperature-sensor-${apiItem.name.toLowerCase()}`,
+        displayName: `BI Temperature Sensor ${apiItem.name}`,
         accessoryType: TemperatureSensorAccessory,
         apiUrl: apiItem.url,
       });
       devices.push({
-        uniqueId: `bi-humidity-sensor-${apiItem.name}`,
-        displayName: 'BI Humidity Sensor',
+        uniqueId: `bi-humidity-sensor-${apiItem.name.toLowerCase()}`,
+        displayName: `BI Humidity Sensor ${apiItem.name}`,
         accessoryType: HumiditySensorAccessory,
         apiUrl: apiItem.url,
       });
       devices.push({
-        uniqueId: `bi-air-quality-sensor-${apiItem.name}`,
-        displayName: 'BI Air Quality Sensor',
+        uniqueId: `bi-air-quality-sensor-${apiItem.name.toLowerCase()}`,
+        displayName: `BI Air Quality Sensor ${apiItem.name}`,
         accessoryType: AirQualitySensorAccessory,
         apiUrl: apiItem.url,
       });
     });
+
+    for (const accessory of this.accessories) {
+      const deviceExists = devices.some(d => this.api.hap.uuid.generate(d.uniqueId) === accessory.UUID);
+      if (!deviceExists) {
+        this.log.info('Removing existing accessory from cache:', accessory.displayName);
+        this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
+      }
+    }
 
     for (const device of devices) {
       const uuid = this.api.hap.uuid.generate(device.uniqueId);
       const existingAccessory = this.accessories.find(accessory => accessory.UUID === uuid);
 
       if (existingAccessory) {
-        if (device) {
-          this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
+        existingAccessory.displayName = device.displayName;
+        this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
 
-          new device.accessoryType(this, existingAccessory, device.apiUrl);
-          this.api.updatePlatformAccessories([existingAccessory]);
-        } else if (!device) {
-          this.log.info('Removing existing accessory from cache:', existingAccessory.displayName);
-          this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [existingAccessory]);
-        }
+        new device.accessoryType(this, existingAccessory, device.apiUrl);
+        this.api.updatePlatformAccessories([existingAccessory]);
       } else {
         this.log.info('Adding new accessory:', device.displayName);
 
